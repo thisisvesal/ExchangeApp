57ff8c0be997edf08fcb92f04fc1ccb9ca2777b1
thisisvesal <vesal.ea84@gmail.com>
2024-06-01 13:44:32 +0430

    Made and loaded up a login page - The icons won't show though

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,23 @@
-import People.Admin;
-import People.Person;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
-public class Main {
-    public static void main(String[] args) {
+public class Main extends Application {
 
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        // TODO Auto-generated method stub
+        try {
+            Parent root = FXMLLoader.load(getClass().getResource("Graphics/LoginPage.fxml"));
+            Scene scene = new Scene(root);
+            primaryStage.setScene(scene);
+            primaryStage.show();
+            scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+        } catch (Exception e) {
+            System.out.println(e.getCause());
+        }
     }
+
 }
--- src/People/User.java
+++ src/People/User.java
@@ -5,4 +5,23 @@ import Transactions.Swap;
 import Transactions.Transfer;
 
 public class User extends Person implements Exchange, Swap, Transfer {
+
+    @Override
+    public void exchange() {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void swap() {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void transfer() {
+        // TODO Auto-generated method stub
+        
+    }
+    
 }
