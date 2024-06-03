818656d6b7bfcf2aac72a7db5e4aed6570c16839
saba <sa7590325@gmail.com>
2024-06-03 15:50:21 +0330

    checking signup info correctly and removing incorrect info from its textFiled

--- src/Authorizaztion/Auth.java
+++ src/Authorizaztion/Auth.java
@@ -5,36 +5,30 @@ import People.Person;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-import Exceptions.InvalidEmailException;
-import Exceptions.InvalidInfoException;
-import Exceptions.InvalidPasswordException;
-import Exceptions.InvalidPhoneNumberException;
-import Exceptions.InvalidUsernameException;
-
 public class Auth {
-   private static boolean uniqueUsername(String username) {
-       for (Person person : Person.getPeople()) {
-           if (person.getUsername().equals(username))
-               return false;
-       }
-       return true;
-   }
+    private static boolean uniqueUsername(String username) {
+        for (Person person : Person.getPeople()) {
+            if (person.getUsername().equals(username))
+                return false;
+        }
+        return true;
+    }
 
-   private static boolean uniqueEmail(String email) {
-       for (Person person : Person.getPeople()) {
-           if (person.getEmail().equals(email))
-               return false;
-       }
-       return true;
-   }
+    private static boolean uniqueEmail(String email) {
+        for (Person person : Person.getPeople()) {
+            if (person.getEmail().equals(email))
+                return false;
+        }
+        return true;
+    }
 
-   private static boolean uniquePhoneNumber(String phoneNumber) {
-       for (Person person : Person.getPeople()) {
-           if (person.getPhoneNumber().equals(phoneNumber))
-               return false;
-       }
-       return true;
-   }
+    private static boolean uniquePhoneNumber(String phoneNumber) {
+        for (Person person : Person.getPeople()) {
+            if (person.getPhoneNumber().equals(phoneNumber))
+                return false;
+        }
+        return true;
+    }
 
     private static boolean isUserNameValid(String UserName) {
         if (!uniqueUsername(UserName)) {
@@ -43,42 +37,27 @@ public class Auth {
         String regex = "^[a-zA-Z0-9]{3,20}$";
         Pattern patten = Pattern.compile(regex);
         Matcher matcher = patten.matcher(UserName);
-        boolean matchFound = matcher.find();
-        if (matchFound) {
-            return true;
-        }
-        return false;
+        return matcher.find();
     }
 
     private static boolean isPasswordValid(String pass) {
         String regex = "^[a-zA-Z0-9]{1,12}$";
         Pattern patten = Pattern.compile(regex);
         Matcher matcher = patten.matcher(pass);
-        boolean matchFound = matcher.find();
-        if (matchFound) {
-            return true;
-        }
-        return false;
+        return matcher.find();
     }
 
     private static boolean doPasswordAndRepeatMatch(String password, String repeat) {
-        if (password.compareTo(repeat) == 0) {
-            return true;
-        }
-        return false;
+        return password.compareTo(repeat) == 0;
     }
 
-    
+
     // TODO: What is this for
     // private static boolean isInputValid(String string) {
     //     String regex = "^[a-zA-Z]{1,18}$";
     //     Pattern patten = Pattern.compile(regex);
     //     Matcher matcher = patten.matcher(string);
-    //     boolean matchFound = matcher.find();
-    //     if (matchFound) {
-    //         return true;
-    //     }
-    //     return false;
+    //     return matcher.find();
     // }
 
     private static boolean isEmailValid(String email) {
@@ -88,11 +67,7 @@ public class Auth {
         String regex = "^[a-zA-Z0-9.]{1,30}@[a-z0-9.]{1,8}(.)[a-z]{1,4}$";
         Pattern patten = Pattern.compile(regex);
         Matcher matcher = patten.matcher(email);
-        boolean matchFound = matcher.find();
-        if (matchFound) {
-            return true;
-        }
-        return false;
+        return matcher.find();
     }
 
     private static boolean isPhoneNumberValid(String phone) {
@@ -102,32 +77,32 @@ public class Auth {
         String regex = "^09[0-9]{9}$";
         Pattern patten = Pattern.compile(regex);
         Matcher matcher = patten.matcher(phone);
-        boolean matchFound = matcher.find();
-        if (matchFound) {
-            return true;
-        }
-        return false;
+        return matcher.find();
     }
 
-    // Weird design here.
-    // I don't know if using exceptions to determine the type of invalid info is a good idea at all
-    public static boolean isInfoValid(String username, String password, String repeatPassword, String email, String phoneNumber) throws InvalidInfoException {
+
+    public static boolean[] isInfoValid(String username, String password, String repeatPassword, String email, String phoneNumber)  {
+        //sw={phoneNumber,email,username,password}
+        boolean[] invalid={false,false,false,false};
+
         if (!isPhoneNumberValid(phoneNumber)) {
-            throw new InvalidPhoneNumberException();
+            invalid[0]=true;
         }
         if (!isEmailValid(email)) {
-            throw new InvalidEmailException();
+            invalid[1]=true;
         }
         if (!isUserNameValid(username)) {
-            throw new InvalidUsernameException();
+            invalid[2]=true;
         }
         if (!isPasswordValid(password)) {
-            throw new InvalidPasswordException();
+            invalid[3]=true;
+        }else {
+            if (!doPasswordAndRepeatMatch(password, repeatPassword)) {
+                invalid[3]=true;
+            }
         }
-        if (!doPasswordAndRepeatMatch(password, repeatPassword)) {
-            throw new InvalidPasswordException();
-        }
+        return invalid;
 
-        return true;
     }
 }
+
