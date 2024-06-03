aa0c428ef33a09d710186a32f078887e599d6dfb
saba <sa7590325@gmail.com>
2024-06-03 14:01:35 +0330

    signup page password textFiled is changed to passwordTextFiled

--- src/Controllers/SignupController.java
+++ src/Controllers/SignupController.java
@@ -1,7 +1,12 @@
 package Controllers;
 
+import Authorizaztion.Auth;
+import Exceptions.*;
+import People.Person;
+import People.User;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
@@ -22,7 +27,6 @@ public class SignupController {
 
     @FXML
     private PasswordField passwordFiled;
-
     @FXML
     private TextField phoneNumberFiled;
 
@@ -31,12 +35,56 @@ public class SignupController {
 
     @FXML
     void getTextInput(ActionEvent event) {
+        try {
+            Auth.isInfoValid(newUsernameField.getText(), passwordFiled.getText(), repeatPasswordField.getText(), emailField.getText(),
+                    phoneNumberFiled.getText());
 
+            User member = new User(newUsernameField.getText(), passwordFiled.getText(), nameField.getText(),
+                    lastNameField.getText(), emailField.getText(), phoneNumberFiled.getText());
+            Person.setCurrentPerson(member);
+            Alert alert = new Alert(Alert.AlertType.INFORMATION);
+            alert.setTitle("Success");
+            alert.setHeaderText("You were signed up successfully.");
+            // alert.setContentText("This is content text.");
+            alert.showAndWait();
+
+        } catch (InvalidUsernameException e) {
+            Alert alert = new Alert(Alert.AlertType.WARNING);
+            alert.setTitle("Invalid info");
+            alert.setHeaderText("Invalid username");
+            alert.setContentText("Please enter a valid username.");
+            alert.showAndWait();
+        } catch (InvalidPasswordException e) {
+            Alert alert = new Alert(Alert.AlertType.WARNING);
+            alert.setTitle("Invalid info");
+            alert.setHeaderText("Invalid password");
+            alert.setContentText("Please enter a valid password.");
+            alert.showAndWait();
+        } catch (InvalidEmailException e) {
+            Alert alert = new Alert(Alert.AlertType.WARNING);
+            alert.setTitle("Invalid info");
+            alert.setHeaderText("Invalid email");
+            alert.setContentText("Please enter a valid email.");
+            alert.showAndWait();
+        } catch (InvalidPhoneNumberException e) {
+            Alert alert = new Alert(Alert.AlertType.WARNING);
+            alert.setTitle("Invalid info");
+            alert.setHeaderText("Invalid phone number");
+            alert.setContentText("Please enter a valid phone number.");
+            alert.showAndWait();
+        } catch (InvalidInfoException e) {
+            Alert alert = new Alert(Alert.AlertType.WARNING);
+            alert.setTitle("Invalid info");
+            alert.setHeaderText("Invalid sign up information");
+            alert.setContentText("Please check your info.");
+            alert.showAndWait();
+        }
     }
 
     @FXML
     void openLoginPage(MouseEvent event) {
-
+        System.out.println("SignUpController - calling SceneController for scene switch");
+        SceneController.getInstance().switchScene("LoginPage.fxml");
     }
 
 }
